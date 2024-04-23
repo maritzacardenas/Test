37801258aae32532773019e2b3451c4c4fa4067f
maritzacardenas <aylin.cardenas@cinvestav.mx>
2024-04-22 23:29:21 -0600

    Second Commit

--- source/main.c
+++ source/main.c
@@ -36,14 +36,9 @@ int main(void) {
 
     PRINTF("Hello World\r\n");
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
+
     while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+
+
+
 }
